9ab4a6f35c37b78a4b6cfb7960cfd0c8ac31eca1
Guilherme Nardari <guivnz@gmail.com>
2018-04-27 11:37:01 -0300

    initial commit

---
+++ solvers/pysocket.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import asyncio
+import datetime
+import random
+import websockets
+import numpy as np
+import time
+import json
+
+IP = 'localhost'
+PORT = 5678
+
+
+def read_config_tsplib(f_name='benchmarks/eil51.tsp'):
+    info = {}
+    cities = []
+    with open(f_name) as f:
+        for i in range(5):
+            line = f.readline().split(':')
+            info[line[0].strip().lower()] = line[1].strip()
+        f.readline()
+
+        for i in range(int(info['dimension'])):
+            line = f.readline().split(' ')
+            cities.append({'x': int(line[1])*10,
+                           'y': int(line[2])*10,
+                           'isDepot': False,
+                           'idx': int(line[0])-1})
+    return {'info': info, 'cities': cities}
+
+async def random_solution(data):
+    '''
+    Simulates a metaheuristic algorithm and returns a random solution
+    '''
+    solution = {'cost': 8000}
+    rand_paths = [i for i in range(len(data['cities']))]
+    np.random.shuffle(rand_paths)
+
+    smanA = rand_paths[:20]
+    smanA.append(smanA[0])
+
+    smanB = rand_paths[20:]
+    smanB.append(smanB[0])
+
+    solution['paths'] = [smanA, smanB]
+    time.sleep(random.random() * 3)
+    return solution
+
+
+data = read_config_tsplib()
+async def sendSolution(websocket, path):
+    while True:
+        data['solution'] = await random_solution(data)
+        await websocket.send(json.dumps(data))
+
+start_server = websockets.serve(sendSolution, IP, PORT)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
---
+++ visualization/tsp.js
@@ -0,0 +1,85 @@
+(function (d3, _) {
+	'use strict';
+	console.log('Started!');
+
+    const colors = ['Blue', 'Black', 'BlueViolet', 'Chartreuse', 'Gold',
+                    'Gray', 'Magenta', 'Red', 'Snow', 'Indigo', 'GreenYellow']
+	var width = 800,
+	    height = 800,
+	    centered,
+	    dotscale = 5,
+	    citiesList = [];
+
+	var svg = d3.select("#tsp").append("svg")
+	    .attr("width", width)
+	    .attr("height", height)
+        .attr("fill", "#5b702a");
+
+	// Arrows
+	svg.append("svg:defs")
+	  .append("svg:marker")
+	    .attr("id", "directed-line")
+	    .attr("viewBox", "0 -5 10 10")
+	    .attr("refX", 15)
+	    .attr("refY", -1.5)
+	    .attr("markerWidth", 6)
+	    .attr("markerHeight", 6)
+	    .attr("orient", "auto")
+	  .append("svg:path")
+	    .attr("d", "M0,-5L10,0L0,5");
+
+	svg.append("rect")
+	    .attr("class", "background")
+	    .attr("width", width)
+	    .attr("height", height)
+        .attr("fill", "#ad744e");
+
+	function drawCities() {
+        svg.selectAll('circle').data(citiesList).enter()
+            .append('circle')
+                .attr('cx', function (d) { return d.x; })
+                .attr('cy', function (d) { return d.y; })
+                .attr('r', dotscale)
+                .attr('class', 'city');
+	}
+
+	function drawPath(ipath, index, drawnPaths) {
+		var paths = _.map(_.zip(ipath.slice(0,ipath.length-1), ipath.slice(1)), function (pair) {
+			return [citiesList[pair[0]], citiesList[pair[1]]]
+		}).slice();
+        
+        drawnPaths = drawnPaths.concat(paths);
+        svg.selectAll('path.connection').data(drawnPaths).enter()
+			.append('path')
+				.attr('d', function(d) {
+			    var dx = d[1].x - d[0].x,
+			        dy = d[1].y - d[0].y,
+			        dr = Math.sqrt(dx * dx + dy * dy);
+			    return "M" + d[0].x + "," + d[0].y + "A" + dr + "," + dr + " 0 0,1 " + d[1].x + "," + d[1].y;
+			  })
+				.attr('class', 'connection')
+				.attr('stroke', colors[index])
+    		.attr("marker-end", "url(#directed-line)");
+
+        return drawnPaths;
+	}
+
+    const ws = new WebSocket("ws://localhost:5678/")
+    ws.onmessage = function (event) {
+        const data = jQuery.parseJSON(event.data);
+        // console.log(data)
+        // drawInfo(data.info)
+        if (data.cities.length > 0 && citiesList.length == 0) {
+            citiesList = data.cities
+            drawCities()
+        }
+
+        svg.selectAll('path.connection').remove();
+        var drawnPaths = [];
+        data.solution.paths.forEach((path, index) => {
+            drawnPaths = drawPath(path, index, drawnPaths)
+        })
+        
+    };
+	console.log('Loaded!');
+})(d3, _);
